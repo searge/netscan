b62f81dcf2eb897f63af5542255af2812e857bd4
Searge <ashikcygan@gmail.com>
2022-01-16 17:36:42 +0200

    Sending ARP request

--- src/smpl/scapy_smpl.py
+++ src/smpl/scapy_smpl.py
@@ -10,6 +10,14 @@ def main():
     # Detailed information
     sc.ls(ether)
 
-    # More detailed information
-    ether.show()
-    ether.display()
+    # For more detailed information
+    # use this commands: `ether.show()` or `ether.display()`
+
+    # Find MAC address via IP address
+    mac_address = sc.ARP(pdst='10.0.0.8')
+
+    # Gather two levels
+    arp_request = ether / mac_address
+    arp_request.show()
+    print(arp_request.layers())
+    print(arp_request.summary())
