7174131440034b6bd471866b442e2451941327e9
Serhii BOREMCHUK <serhii.boremchuk@smile-ukraine.com>
2022-01-17 12:11:08 +0200

    ARP Scanner | working copy

--- src/arp_scanner.py
+++ src/arp_scanner.py
@@ -6,22 +6,28 @@ from scapy import config as conf
 conf.verb = 0
 
 
-@click.option('--ip', required=True)
+@click.option('--ip', required=True, multiple=True, type=(str))
 @click.command('arp')
 def scan_local_network(ip):
     """
     Scan local network.
 
     Args:
-        ip (str): Destintaion IP
+        ip (Tuple[str]): Destintaion IP
     """
-    arp_layer = sc.ARP(pdst=ip)
     ether_layer = sc.Ether(dst='ff:ff:ff:ff:ff:ff')
-    arp_request = ether_layer / arp_layer
 
-    answered, _ = sc.srp(arp_request, timeout=10)
+    for address in ip:
+        arp_layer = sc.ARP(pdst=address)
+        arp_request = ether_layer / arp_layer
 
-    if answered:
-        print(f'host {ip} available')
-    else:
-        print(f'host {ip} not available')
+        answered = sc.srp(arp_request, timeout=10)[0]
+
+        if answered:
+            click.echo(f'{address} | available')
+        else:
+            click.echo(f'{address} | not available')
+
+
+if __name__ == '__main__':
+    scan_local_network()
